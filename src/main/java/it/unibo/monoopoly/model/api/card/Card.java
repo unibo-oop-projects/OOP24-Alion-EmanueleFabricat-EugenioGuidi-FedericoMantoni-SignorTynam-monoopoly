86e0506ec8d6a0832e21741262055c5d5563627a
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-01-15 11:04:57 +0100

    add java doc in Card

--- src/main/java/it/unibo/monoopoly/model/api/card/Card.java
+++ src/main/java/it/unibo/monoopoly/model/api/card/Card.java
@@ -1,7 +1,23 @@
 package it.unibo.monoopoly.model.api.card;
 
+/**
+ * The interface that implement the resolution of draw a card.
+ * The card must apply the effects itself to the player who drew the card.
+ */
 public interface Card {
+    /**
+     * This method deals with applying effects,
+     * if they don't are about cash.
+     */
     public void doAction();
+    /**
+     * This method deals with applying effects,
+     * if they are about cash.
+     * @return the amount to be pay or collected
+     */
     public int getValue();
+    /**
+     * @return the text of the card.
+     */
     public String getText();
 }
