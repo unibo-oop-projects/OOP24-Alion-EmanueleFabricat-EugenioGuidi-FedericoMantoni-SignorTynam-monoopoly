11cc27293580ebc051b6985b4f5c99bd3431a7b5
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-01-15 11:16:39 +0100

    add java doc in Banker

--- src/main/java/it/unibo/monoopoly/model/api/Banker.java
+++ src/main/java/it/unibo/monoopoly/model/api/Banker.java
@@ -1,5 +1,13 @@
 package it.unibo.monoopoly.model.api;
 
+/**
+ * Interface that implements a benker who help the player decide wich house sell or properties mortagage.
+ */
 public interface Banker {
+    /**
+     * Make with the palyer the operationes.
+     * 
+     * @return the proceeds of the oparationes.
+     */
     public int makeOperations ();
 }
