0a268b431c361c2b91ce5dd44545cfca016f3ea6
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-01-15 10:46:19 +0100

    add java doc in Deck

--- src/main/java/it/unibo/monoopoly/model/api/card/Deck.java
+++ src/main/java/it/unibo/monoopoly/model/api/card/Deck.java
@@ -1,6 +1,18 @@
 package it.unibo.monoopoly.model.api.card;
 
+/**
+ * The interface that implemented the deck of cards,
+ * and its management
+ */
 public interface Deck {
+    /**
+     * method that shuffle the card deck
+     * 
+     */
     public void shuffleDeck();
-    public Card draw(Player); 
+    /**
+     * @param player, who drawn the card.
+     * 
+     */
+    public void draw(Player player); 
 }
