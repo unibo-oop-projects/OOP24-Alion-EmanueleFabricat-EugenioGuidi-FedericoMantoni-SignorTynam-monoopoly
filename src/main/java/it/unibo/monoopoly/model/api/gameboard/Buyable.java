0eb7f11dac366bc5b609eefd55e1ec49f8af9340
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-01-17 20:17:24 +0100

    add interface pair, removed public from method of interfaces Dice, Bildable, Buyable and GameBoard

--- src/main/java/it/unibo/monoopoly/model/api/gameboard/Buyable.java
+++ src/main/java/it/unibo/monoopoly/model/api/gameboard/Buyable.java
@@ -2,6 +2,8 @@ package it.unibo.monoopoly.model.api.gameboard;
 
 import java.util.Optional;
 
+import it.unibo.monoopoly.model.api.player.Player;
+
 public interface Buyable {
 
     /**
@@ -9,47 +11,47 @@ public interface Buyable {
      * 
      * @return true if is buyable
      */
-    public boolean isBuyable();
+    boolean isBuyable();
 
     /**
      * method that return the owner of property.
      * 
      * @return the owner of property
      */
-    public Optional<Player> getOwner();
+    Optional<Player> getOwner();
 
     /**
      * method that return the cost of property.
      * 
      * @return the cost of property
      */
-    public int getCost();
+    int getCost();
 
     /**
      * method that set the owner of property.
      * 
      * @param ownerPlayer
      */
-    public void setOwner(Optional<player> ownerPlayer);
+    void setOwner(Optional<Player> ownerPlayer);
 
     /**
      * method that return the rent of ther property.
      * 
      * @return the value of rent the property
      */
-    public int getTRentalValue();
+    int getTRentalValue();
 
     /**
      * method that set the mortgage of property.
      * 
      */
-    public void mortgage();
+    void mortgage();
 
     /**
      * method that return the mortgage value.
      * 
      * @return the mortgage value
      */
-    public int getMortgageValue();
+    int getMortgageValue();
 
 }
