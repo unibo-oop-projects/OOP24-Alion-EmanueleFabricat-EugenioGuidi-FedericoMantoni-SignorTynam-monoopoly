69aaad9f2b23415e9bcb68f2c11a141d7fd03a29
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-01-22 19:44:47 +0100

    add class of that implements the interfaces Deck, Card and Banker

---
+++ src/main/java/it/unibo/monoopoly/model/impl/card/DeckImpl.java
@@ -0,0 +1,20 @@
+package it.unibo.monoopoly.model.impl.card;
+
+import it.unibo.monoopoly.model.api.card.Deck;
+import it.unibo.monoopoly.model.api.player.Player;
+
+public class DeckImpl  implements Deck{
+
+    @Override
+    public void shuffleDeck() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'shuffleDeck'");
+    }
+
+    @Override
+    public void draw(Player player) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    }
+
+}
