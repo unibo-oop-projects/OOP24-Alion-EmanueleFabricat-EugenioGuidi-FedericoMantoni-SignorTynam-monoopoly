52d726323834ca34f401c391175ab54af7838b32
Federico Mantoni <federico.mantoni@studio.unibo.it>
2025-01-22 16:54:03 +0100

    minor fixes in Notary and Functional interfaces, add methods to Cell interface

--- src/main/java/it/unibo/monoopoly/model/api/Notary.java
+++ src/main/java/it/unibo/monoopoly/model/api/Notary.java
@@ -4,12 +4,12 @@ import it.unibo.monoopoly.model.api.gameboard.Buyable;
 import it.unibo.monoopoly.model.api.player.Player;
 
 /**
- * Manage the operations of buying properties and pay
+ * Manage the operations of buying properties and pay.
  */
 public interface Notary {
 
     /**
-     * Tells if a property is owned by a {@code Player} different from a given one
+     * Tells if a property is owned by a {@code Player} different from a given one.
      * @param player the player to check
      * @param cell the cell of the property to check
      * @return if the property is not owned by the given player nor by the bank
@@ -17,12 +17,10 @@ public interface Notary {
     boolean isOtherProperty(Player player, Buyable cell);
 
     /**
-     * Set the given player as owner of the property
+     * Set the given player as owner of the property.
      * @param player the player who buys
      * @param cell the property to buy
      */
     void buyProperty(Player player, Buyable cell);
-
-    
 
 }
