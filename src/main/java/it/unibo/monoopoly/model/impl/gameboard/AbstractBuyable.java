88d7ea4996ae0a4cbedf4d3d97cb8fc8ec680653
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-01-23 12:34:12 +0100

    add unimplemented method of intarface buyable

--- src/main/java/it/unibo/monoopoly/model/impl/gameboard/AbstractBuyable.java
+++ src/main/java/it/unibo/monoopoly/model/impl/gameboard/AbstractBuyable.java
@@ -1,5 +1,52 @@
 package it.unibo.monoopoly.model.impl.gameboard;
 
-public abstract class AbstractBuyable {
+import java.util.Optional;
+
+import it.unibo.monoopoly.model.api.gameboard.Buyable;
+import it.unibo.monoopoly.model.api.player.Player;
+
+public abstract class AbstractBuyable implements Buyable{
+
+    @Override
+    public int getCost() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getMortgageValue() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Optional<Player> getOwner() {
+        // TODO Auto-generated method stub
+        return Optional.empty();
+    }
+
+    @Override
+    public int getTRentalValue() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isBuyable() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void mortgage() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setOwner(Optional<Player> ownerPlayer) {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
