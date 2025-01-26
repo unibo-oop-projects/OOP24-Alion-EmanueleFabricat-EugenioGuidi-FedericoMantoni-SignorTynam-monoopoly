3c95b69b128b657cd9ac1eab53980f4232aa9061
alion <alion.emini2@studio.unibo.it>
2025-01-26 18:08:20 +0100

    add PlayerImpl class for player functionality [funzioni non implementate]

---
+++ src/main/java/it/unibo/monoopoly/model/impl/player/PlayerImpl.java
@@ -0,0 +1,49 @@
+package it.unibo.monoopoly.model.impl.player;
+
+import it.unibo.monoopoly.model.api.player.Player;
+
+public class PlayerImpl implements Player {
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
+
+    @Override
+    public int getMoneyAmount() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMoneyAmount'");
+    }
+
+    @Override
+    public int getActualPosition() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getActualPosition'");
+    }
+
+    @Override
+    public boolean isPrisoned() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isPrisoned'");
+    }
+
+    @Override
+    public boolean isPayable(int amount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isPayable'");
+    }
+
+    @Override
+    public void pay(int amount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pay'");
+    }
+
+    @Override
+    public void receive(int amount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'receive'");
+    }
+
+}
