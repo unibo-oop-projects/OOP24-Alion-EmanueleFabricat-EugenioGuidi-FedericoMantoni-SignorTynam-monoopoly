9fb9e2b96ad771adb698ce21b158f241bc8a665c
alion <alion.emini2@studio.unibo.it>
2025-01-26 18:18:05 +0100

    basic initializatio of PlayerImpl. add PlayerImpl constructor with validation for name, money, position, and prison status

--- src/main/java/it/unibo/monoopoly/model/impl/player/PlayerImpl.java
+++ src/main/java/it/unibo/monoopoly/model/impl/player/PlayerImpl.java
@@ -1,9 +1,27 @@
 package it.unibo.monoopoly.model.impl.player;
 
+import java.util.Optional;
+
 import it.unibo.monoopoly.model.api.player.Player;
 
 public class PlayerImpl implements Player {
 
+    private final String name;
+    private int moneyAmount;
+    private int actualPosition;
+    private boolean prisoned;
+
+    public PlayerImpl(String name, int moneyAmount, int actualPosition, boolean prisoned) {
+        this.name = Optional.ofNullable(name).orElseThrow(() -> new IllegalArgumentException("Name cannot be null"));
+        this.moneyAmount = validatePositive(moneyAmount, "Money amount cannot be negative");
+        this.actualPosition = validatePositive(actualPosition, "Position cannot be negative");
+                this.prisoned = prisoned;
+    }
+        
+    private int validatePositive(int value, String errorMessage) {
+        return Optional.of(value).filter(i -> i >= 0).orElseThrow(() -> new IllegalArgumentException(errorMessage));
+    }
+        
     @Override
     public String getName() {
         // TODO Auto-generated method stub
