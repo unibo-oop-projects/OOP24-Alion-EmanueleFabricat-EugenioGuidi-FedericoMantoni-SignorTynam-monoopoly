7fcc7ce5a5821a9f3e93fcd3768006c6e86a3c58
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-01-26 14:43:59 +0100

    add test testOwner in class BuildableImplTest

--- src/test/java/it/unibo/monoopoly/BuildableImplTest.java
+++ src/test/java/it/unibo/monoopoly/BuildableImplTest.java
@@ -1,15 +1,22 @@
 package it.unibo.monoopoly;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.lang.foreign.Linker.Option;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import it.unibo.monoopoly.model.api.player.Player;
 import it.unibo.monoopoly.model.impl.gameboard.BuildableImpl;
 
 public class BuildableImplTest {
 
     static final int COST = 100;
+    static final Optional<Player> FIRSTOWNER = new PlayerImpl();
+    static final Optional<Player> SECONDOWNER = new PlayerImpl();
 
     private BuildableImpl property;
 
@@ -23,4 +30,16 @@ public class BuildableImplTest {
         assertEquals(COST, property.getCost());
     }
 
+    @Test
+    public void testOwner() {
+        assertEquals(Optional.empty(), this.property.getOwner());
+        this.property.setOwner(FIRSTOWNER);
+        assertNotEquals(Optional.empty(), this.property.getOwner());
+        assertNotEquals(SECONDOWNER, this.property.getOwner());
+        assertEquals(FIRSTOWNER, this.property.getOwner());
+        this.property.setOwner(Optional.empty());
+        assertNotEquals(FIRSTOWNER, this.property.getOwner());
+        assertNotEquals(SECONDOWNER, this.property.getOwner());
+        assertEquals(Optional.empty(), this.property.getOwner());
+    }
 }
