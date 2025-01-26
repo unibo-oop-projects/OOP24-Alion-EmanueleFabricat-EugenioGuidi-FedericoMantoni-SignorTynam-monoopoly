b8e543462cbc4e25f1b8b9342cc1d3e24501197e
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-01-26 14:30:25 +0100

    add test testGetCost in class BuildableImplTest

--- src/test/java/it/unibo/monoopoly/BuildableImplTest.java
+++ src/test/java/it/unibo/monoopoly/BuildableImplTest.java
@@ -1,16 +1,26 @@
 package it.unibo.monoopoly;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import it.unibo.monoopoly.model.impl.gameboard.BuildableImpl;
 
 public class BuildableImplTest {
 
+    static final int COST = 100;
+
     private BuildableImpl property;
 
     @BeforeEach
     public void initialization() {
-        this.property = new BuildableImpl(100);
+        this.property = new BuildableImpl(COST);
+    }
+
+    @Test
+    public void testGetCost() {
+        assertEquals(COST, property.getCost());
     }
 
 }
