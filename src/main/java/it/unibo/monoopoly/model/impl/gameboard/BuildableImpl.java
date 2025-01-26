feca81fa4f13d58127e7b91e9dac4f88cd605005
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-01-26 11:54:51 +0100

    add unimplemented method to class BuildableImpl

--- src/main/java/it/unibo/monoopoly/model/impl/gameboard/BuildableImpl.java
+++ src/main/java/it/unibo/monoopoly/model/impl/gameboard/BuildableImpl.java
@@ -1,5 +1,43 @@
 package it.unibo.monoopoly.model.impl.gameboard;
 
-public class BuildableImpl {
+import it.unibo.monoopoly.model.api.gameboard.Buildable;
+
+public class BuildableImpl extends AbstractBuyable implements Buildable{
+
+    @Override
+    public int getRentalValue() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRentalValue'");
+    }
+
+    @Override
+    public boolean isBuildable() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isBuildable'");
+    }
+
+    @Override
+    public int getHouseCost() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHouseCost'");
+    }
+
+    @Override
+    public void buildHouse() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buildHouse'");
+    }
+
+    @Override
+    public int sellHouse() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sellHouse'");
+    }
+
+    @Override
+    public int getHousesNumber() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHousesNumber'");
+    }
 
 }
