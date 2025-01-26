b531dc30b072c1afdca6c5fa8fca26f79ff48215
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-01-26 13:03:19 +0100

    in class BuildableImplTest create inizialization of property for each future test

--- src/test/java/it/unibo/monoopoly/BuildableImplTest.java
+++ src/test/java/it/unibo/monoopoly/BuildableImplTest.java
@@ -1,5 +1,16 @@
 package it.unibo.monoopoly;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import it.unibo.monoopoly.model.impl.gameboard.BuildableImpl;
+
 public class BuildableImplTest {
+
+    private BuildableImpl property;
+
+    @BeforeEach
+    public void initialization() {
+        this.property = new BuildableImpl(100);
+    }
 
 }
