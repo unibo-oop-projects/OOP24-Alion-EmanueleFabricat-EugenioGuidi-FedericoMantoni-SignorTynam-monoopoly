9325241ec7b93e54337dc9ff914dc7a08b543267
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-01-27 20:45:33 +0100

    in class DiceImpl add unimplemented method

--- src/main/java/it/unibo/monoopoly/model/impl/gameboard/DiceImpl.java
+++ src/main/java/it/unibo/monoopoly/model/impl/gameboard/DiceImpl.java
@@ -1,5 +1,13 @@
 package it.unibo.monoopoly.model.impl.gameboard;
 
-public class DiceImpl {
+import it.unibo.monoopoly.model.api.gameboard.Dice;
+
+public class DiceImpl implements Dice{
+
+    @Override
+    public Pair<Integer, Integer> rollDice() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'rollDice'");
+    }
 
 }
