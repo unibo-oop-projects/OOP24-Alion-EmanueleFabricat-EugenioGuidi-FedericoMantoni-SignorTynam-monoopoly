6c2c71ac9dd36f4eabcbfd6a32cc853f67c063d5
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-01-27 20:44:28 +0100

    in interface Dice add record Pair

--- src/main/java/it/unibo/monoopoly/model/api/gameboard/Dice.java
+++ src/main/java/it/unibo/monoopoly/model/api/gameboard/Dice.java
@@ -2,6 +2,8 @@ package it.unibo.monoopoly.model.api.gameboard;
 
 public interface Dice {
 
+    record Pair<T, U> (T firstRoll, U secondRoll){}
+
     /**
      * 
      * @return the roll of the dice
