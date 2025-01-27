e7f2fdbbb8a5aadd1078b2c477c76bbc9cbf4bbd
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-01-27 02:12:10 +0100

    implemented the private method haveHouse in BankerImpl

--- src/main/java/it/unibo/monoopoly/model/impl/BankerImpl.java
+++ src/main/java/it/unibo/monoopoly/model/impl/BankerImpl.java
@@ -1,6 +1,10 @@
 package it.unibo.monoopoly.model.impl;
 
+import java.util.Set;
+
 import it.unibo.monoopoly.model.api.Banker;
+import it.unibo.monoopoly.model.api.gameboard.Buildable;
+import it.unibo.monoopoly.model.api.gameboard.Buyable;
 import it.unibo.monoopoly.model.api.player.Player;
 
 public class BankerImpl implements Banker{
@@ -8,10 +12,17 @@ public class BankerImpl implements Banker{
     @Override
     public int selectOperations(Player player, int amount) {
         if (player.isPayable(amount)) {
-            
+
         } else {
             makeChoose(haveHouse(player.getProperties()) || haveProperties(player.getProperties()));
         }
+    }
+
+    private boolean haveHouse(Set<Buyable> propreties) {
+        return propreties.stream()
+                .filter(p -> p instanceof Buildable)
+                .map(p -> (Buildable)p)
+                .anyMatch(p -> p.getHousesNumber() != 0);
     }
 
 }
