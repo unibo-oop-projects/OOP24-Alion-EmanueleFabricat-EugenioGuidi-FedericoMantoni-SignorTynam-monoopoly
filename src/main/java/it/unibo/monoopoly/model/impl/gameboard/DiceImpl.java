115db69d82ac028eb0b8719301452b08d683d7fb
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-01-28 11:51:41 +0100

    in class DiceImpl add constructor

--- src/main/java/it/unibo/monoopoly/model/impl/gameboard/DiceImpl.java
+++ src/main/java/it/unibo/monoopoly/model/impl/gameboard/DiceImpl.java
@@ -6,6 +6,12 @@ import it.unibo.monoopoly.model.api.gameboard.Dices;
 
 public class DiceImpl implements Dices{
 
+    private Optional<Pair> currentRoll;
+
+    public DiceImpl() {
+        this.currentRoll = Optional.empty();
+    }
+
     @Override
     public Void rollDices() {
         // TODO Auto-generated method stub
