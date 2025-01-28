ae1c8551c63730ff55029d57ac02ccbd5c226013
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-01-28 17:40:40 +0100

    in class GameBoardImpl add two private static method to initialize CELLS_LIST and PLAYERS_LIST

--- src/test/java/it/unibo/monoopoly/model/gameboard/GameboardImplTest.java
+++ src/test/java/it/unibo/monoopoly/model/gameboard/GameboardImplTest.java
@@ -1,18 +1,46 @@
 package it.unibo.monoopoly.model.gameboard;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.jupiter.api.BeforeEach;
 
+import it.unibo.monoopoly.model.api.gameboard.Cell;
 import it.unibo.monoopoly.model.api.gameboard.GameBoard;
+import it.unibo.monoopoly.model.api.player.Player;
+import it.unibo.monoopoly.model.impl.gameboard.BuildableImpl;
 import it.unibo.monoopoly.model.impl.gameboard.GameBoardImpl;
+import it.unibo.monoopoly.model.impl.player.PlayerImpl;
 
 
 public class GameBoardImplTest {
 
+    static final List<Cell> CELLS_LIST = GameBoardImplTest.initializeCellsList();
+    static final List<Player> PLAYERS_LIST = GameBoardImplTest.initializePlayersList();
+
     private GameBoard gameBoardImpl;
+
+    private static List<Cell> initializeCellsList() {
+        final List<Cell> cellsList = new LinkedList<>();
+        cellsList.add(new BuildableImpl(null, "first", 100, 100));
+        cellsList.add(new BuildableImpl(null, "second", 200, 200));
+        cellsList.add(new BuildableImpl(null, "third", 300, 300));
+        return cellsList;
+    }
+
+    private static List<Player> initializePlayersList() {
+        final List<Player> playersList = new LinkedList<>();
+        playersList.add(new PlayerImpl("Mario", 1500, 0, false));
+        playersList.add(new PlayerImpl("Franco", 1500, 0, false));
+        playersList.add(new PlayerImpl("Luigi", 1500, 0, false));
+        return playersList;
+    }
 
     @BeforeEach
     public void initialization() {
-        this.gameBoardImpl = new GameBoardImpl();
+        this.gameBoardImpl = new GameBoardImpl(CELLS_LIST, PLAYERS_LIST);
     }
 
 }
