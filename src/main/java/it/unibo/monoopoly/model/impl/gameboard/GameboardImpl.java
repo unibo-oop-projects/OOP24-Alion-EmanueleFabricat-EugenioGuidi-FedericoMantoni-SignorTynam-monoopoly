49de652d42daea9f0f2a4b476ceb75a199195bf7
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-01-28 18:36:08 +0100

    in class GameBoardImpl add fields and constructor

--- src/main/java/it/unibo/monoopoly/model/impl/gameboard/GameboardImpl.java
+++ src/main/java/it/unibo/monoopoly/model/impl/gameboard/GameboardImpl.java
@@ -1,9 +1,22 @@
 package it.unibo.monoopoly.model.impl.gameboard;
 
+import java.util.List;
+
 import it.unibo.monoopoly.model.api.gameboard.Cell;
 import it.unibo.monoopoly.model.api.gameboard.GameBoard;
+import it.unibo.monoopoly.model.api.player.Player;
 
 public class GameBoardImpl implements GameBoard{
+
+    private final List<Cell> cellsList;
+    private final List<Player> playersList;
+    private int currentPlayer;
+    
+    public GameBoardImpl(final List<Cell> cellsList, List<Player> playersList) {
+        this.cellsList = cellsList;
+        this.playersList = playersList;
+        this.currentPlayer = 0;
+    }
 
     @Override
     public Cell getCell(int index) {
