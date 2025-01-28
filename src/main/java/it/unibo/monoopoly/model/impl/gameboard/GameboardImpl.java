430dc247867414fc36336032f0d6bf8fe4531739
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-01-28 16:29:53 +0100

    in class GameboardImpl ad unimplemented method

--- src/main/java/it/unibo/monoopoly/model/impl/gameboard/GameboardImpl.java
+++ src/main/java/it/unibo/monoopoly/model/impl/gameboard/GameboardImpl.java
@@ -1,5 +1,26 @@
-package it.unibo.monoopoly.model.gameboard;
+package it.unibo.monoopoly.model.impl.gameboard;
 
-public class BoardgameImpl {
+import it.unibo.monoopoly.model.api.gameboard.Cell;
+import it.unibo.monoopoly.model.api.gameboard.GameBoard;
+
+public class GameboardImpl implements GameBoard{
+
+    @Override
+    public Cell getCell(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCell'");
+    }
+
+    @Override
+    public void removePlayer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removePlayer'");
+    }
+
+    @Override
+    public boolean isGameEnded() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isGameEnded'");
+    }
 
 }
