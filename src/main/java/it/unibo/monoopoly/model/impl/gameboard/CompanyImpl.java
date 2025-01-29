e2f4853842ce693122ca5e0cef9203a0d7c70054
Federico Mantoni <federico.mantoni@studio.unibo.it>
2025-01-29 01:25:28 +0100

    Implement methods of CompanyImpl

--- src/main/java/it/unibo/monoopoly/model/impl/gameboard/CompanyImpl.java
+++ src/main/java/it/unibo/monoopoly/model/impl/gameboard/CompanyImpl.java
@@ -1,14 +1,17 @@
 package it.unibo.monoopoly.model.impl.gameboard;
 
-import it.unibo.monoopoly.model.api.card.Dice;
+import java.util.Optional;
+
 import it.unibo.monoopoly.model.api.gameboard.Company;
+import it.unibo.monoopoly.model.api.gameboard.Dices;
 
 /**
  * Implements the {@link Company} interface.
  */
 public class CompanyImpl extends AbstractBuyable implements Company {
 
-    private Dice dice;
+    private final Dices dice;
+    private Optional<Integer> actualRentalValue;
 
     /**
      * Constructor that will be replaced by CellFactory.
@@ -17,15 +20,17 @@ public class CompanyImpl extends AbstractBuyable implements Company {
      */
     public CompanyImpl(final String name, final int cost) {
             super(name, cost);
+            this.dice = new DicesImpl();
+            this.actualRentalValue = Optional.empty();
         }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public int getRentalValue() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRentalValue'");
+    public int calculateRentalValue() {
+        return this.actualRentalValue.orElseThrow(() ->
+            new IllegalStateException("Rental value need to be first calculated for Companies"));
     }
 
     /**
@@ -33,8 +38,15 @@ public class CompanyImpl extends AbstractBuyable implements Company {
      */
     @Override
     public void rollAndCalculate() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'calculateRent'");
+        this.dice.rollDices();
+        final int multiplier = this.hasAnotherCompany() ? 10 : 4;
+        this.actualRentalValue=multiplier*this.dice.getResult();
+    }
+
+    private boolean hasAnotherCompany() {
+        return Math.toIntExact(
+            this.getOwner().get().getProperties().stream()
+            .filter(p -> p.isCompany()).count()) == 2;
     }
 
 }
