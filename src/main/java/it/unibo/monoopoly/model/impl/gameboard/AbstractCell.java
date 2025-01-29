f937d94278fb24fdabb96631296054ef5ab2922f
Federico Mantoni <federico.mantoni@studio.unibo.it>
2025-01-30 00:05:11 +0100

    first changes to AbstractCell needed for JsonConverter

--- src/main/java/it/unibo/monoopoly/model/impl/gameboard/AbstractCell.java
+++ src/main/java/it/unibo/monoopoly/model/impl/gameboard/AbstractCell.java
@@ -1,8 +1,19 @@
 package it.unibo.monoopoly.model.impl.gameboard;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 import it.unibo.monoopoly.model.api.gameboard.Buildable;
 import it.unibo.monoopoly.model.api.gameboard.Buyable;
 import it.unibo.monoopoly.model.api.gameboard.Cell;
+
+@JsonTypeInfo(
+use = JsonTypeInfo.Id.NAME,
+include = JsonTypeInfo.As.PROPERTY,
+property = "type") // field name
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = Buildable.class, name = "Buildable")
+})
 
 /**
  * Abstract class implementing common method of a generic {@link Cell}.
