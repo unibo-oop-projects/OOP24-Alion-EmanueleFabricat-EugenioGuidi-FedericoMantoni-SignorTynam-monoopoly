c7ec2d25d49d2e621c15bf2f8af998e247ae4e59
Federico Mantoni <federico.mantoni@studio.unibo.it>
2025-01-30 21:25:23 +0100

    Update test

--- src/test/java/TestJsonConverter.java
+++ src/test/java/TestJsonConverter.java
@@ -2,18 +2,18 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-import it.unibo.monoopoly.model.api.gameboard.Buyable;
+import it.unibo.monoopoly.model.api.gameboard.Cell;
 import it.unibo.monoopoly.utils.api.JsonConverter;
 import it.unibo.monoopoly.utils.impl.JsonConverterImpl;
 
 public class TestJsonConverter {
 
-    private JsonConverter<Buyable> converter;
-    private List<Buyable> result;
+    private JsonConverter<Cell> converter;
+    private List<Cell> result;
 
     @Test
     void testConversion() {
-        this.converter = new JsonConverterImpl<>(Buyable.class);
+        this.converter = new JsonConverterImpl<>(Cell.class);
         result = converter.jsonToList(ClassLoader.getSystemResourceAsStream("cells.json"));
     }
 
