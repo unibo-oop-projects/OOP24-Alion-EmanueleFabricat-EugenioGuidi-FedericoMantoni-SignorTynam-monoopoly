83ce6dcf19fbdd563fa8c5f8dc5740f49a0cd1b7
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-02-03 00:26:36 +0100

    start implementation of Deck

--- src/main/java/it/unibo/monoopoly/model/impl/card/DeckImpl.java
+++ src/main/java/it/unibo/monoopoly/model/impl/card/DeckImpl.java
@@ -1,20 +1,32 @@
 package it.unibo.monoopoly.model.impl.card;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import it.unibo.monoopoly.model.api.card.Card;
 import it.unibo.monoopoly.model.api.card.Deck;
 import it.unibo.monoopoly.model.api.player.Player;
 
 public class DeckImpl  implements Deck{
+    List<Card> deck = new LinkedList<>();
+    Set<Card> discardPile = new HashSet<>();
 
     @Override
     public void shuffleDeck() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'shuffleDeck'");
+        this.deck.addAll(this.discardPile);
+        this.discardPile.removeAll(this.discardPile);
+        Collections.shuffle(this.deck);
     }
 
     @Override
     public void draw(Player player) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+        /*if (this.deck.isEmpty()) {
+            this.shuffleDeck();
+        }*/
+        this.deck.removeFirst();
     }
 
 }
