9370334e04ee4e407261fc31b5302257c82d4e9c
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-02-03 00:31:44 +0100

    start implementing Card

--- src/main/java/it/unibo/monoopoly/model/impl/card/CardImpl.java
+++ src/main/java/it/unibo/monoopoly/model/impl/card/CardImpl.java
@@ -1,8 +1,16 @@
 package it.unibo.monoopoly.model.impl.card;
 
 import it.unibo.monoopoly.model.api.card.Card;
+import it.unibo.monoopoly.utils.Message;
 
 public class CardImpl implements Card{
+    private final String effect;
+    private final Message action;
+
+    public CardImpl(String effect, Message action) {
+        this.effect = effect;
+        this.action = action;
+    }
 
     @Override
     public void doAction() {
@@ -12,8 +20,7 @@ public class CardImpl implements Card{
 
     @Override
     public String getText() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getText'");
+        return this.effect;
     }
 
 }
