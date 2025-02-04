9180087ca86a2a9b41c179254fa791342c299e05
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-02-05 00:24:43 +0100

    update BankerImpl, with the last modify of the interface

--- src/main/java/it/unibo/monoopoly/model/impl/BankerImpl.java
+++ src/main/java/it/unibo/monoopoly/model/impl/BankerImpl.java
@@ -1,8 +1,10 @@
 package it.unibo.monoopoly.model.impl;
 
+import java.lang.foreign.Linker.Option;
 import java.util.List;
-
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import it.unibo.monoopoly.common.Event;
 import it.unibo.monoopoly.model.api.Banker;
@@ -17,54 +19,70 @@ public class BankerImpl implements Banker {
      * {@inheritDoc}
      */
     @Override
-    public Event selectOperations(final Player player, final int amount) {
+    public Optional<Event> selectOperations(final Player player, final int amount) {
         if (player.isPayable(amount)) {
             player.pay(amount);
-            return Event.;
+            return Optional.empty();
         } else {
             if (haveHouse(player.getProperties())) {
-                return Event.;
+                return Event.SELL_HOUSE;
             } else if (haveProperties(player.getProperties())) {
-                return Event.;
-            } else {
-                return Event.;
+                return Event.MORTGAGE_PROPERTY;
             }
         }
+        player.inBankrupt(); 
+        return Optional.empty();
     }
 
-    private boolean haveHouse(final Set<Buyable> propreties) {
-        return propreties.stream()
+    private Stream<Buildable> buildableList(final Set<Buyable> properties) {
+        return properties.stream()
                 .filter(p -> p instanceof Buildable)
-                .map(p -> (Buildable) p)
+                .filter(p -> !p.isMortgaged())
+                .map(p -> (Buildable) p);
+    }
+
+    private boolean haveHouse(final Set<Buyable> properties) {
+        return buildableList(properties)
                 .anyMatch(p -> p.getHousesNumber() != 0);
     }
 
-    private boolean haveProperties(final Set<Buyable> propreties) {
-        return !propreties.isEmpty();
+    private boolean haveProperties(final Set<Buyable> properties) {
+        return buildableList(properties)
+                .count() > 0;
     }
-
-    private List<Buildable> getPropretiesWithHome(final Set<Buyable> propreties) {
-        return propreties.stream()
-                .filter(p -> p instanceof Buildable)
-                .map(p -> (Buildable) p)
+    /**
+     * {@inheritDoc}
+     */
+    public List<Buildable> getPropertiesWithHome(Player player) {
+        return buildableList(player.getProperties())
                 .filter(p -> p.getHousesNumber() > 0)
                 .toList();
     }
     /**
+     * 
      * {@inheritDoc}
      */
     @Override
-    public void sellHouse(final Buildable proprety, final Player player) {
-        player.receive(proprety.sellHouse());
+    public List<Buildable> getPropertiesMortgageable(Player player) {
+        return buildableList(player.getProperties()).toList();
     }
     /**
      * {@inheritDoc}
      */
     @Override
-    public void mortgageProprety(final Buyable proprety, final Player player) {
-        player.receive(proprety.getMortgageValue());
-        proprety.setMortgage();
+    public void sellHouse(final Buildable property, final Player player) {
+        player.receive(property.sellHouse());
     }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void mortgageProperty(final Buyable property, final Player player) {
+        player.receive(property.getMortgageValue());
+        property.setMortgage();
+    }
+
+    
 
     
 }
