4b6142587ef49737c02785319a7b3a6dc2e53623
Federico Mantoni <federico.mantoni@studio.unibo.it>
2025-02-05 00:38:44 +0100

    move PanelAdapter to impl folder

--- src/main/java/it/unibo/monoopoly/view/impl/PanelAdapter.java
+++ src/main/java/it/unibo/monoopoly/view/impl/PanelAdapter.java
@@ -1,6 +1,8 @@
-package it.unibo.monoopoly.view.api;
+package it.unibo.monoopoly.view.impl;
 
 import javax.swing.JPanel;
+
+import it.unibo.monoopoly.view.api.MyPanel;
 
 public abstract class PanelAdapter extends JPanel implements MyPanel {
 
