f3d994fc6bfbba2c8c3910c26ccdadab0088905c
Federico Mantoni <federico.mantoni@studio.unibo.it>
2025-02-04 18:14:16 +0100

    create first implementation of CellFactory

---
+++ src/main/java/it/unibo/monoopoly/model/impl/gameboard/CellFactoryImpl.java
@@ -0,0 +1,27 @@
+package it.unibo.monoopoly.model.impl.gameboard;
+
+import java.util.List;
+
+import it.unibo.monoopoly.model.api.gameboard.Cell;
+import it.unibo.monoopoly.model.api.gameboard.CellFactory;
+import it.unibo.monoopoly.utils.api.JsonConverter;
+import it.unibo.monoopoly.utils.impl.JsonConverterImpl;
+
+public class CellFactoryImpl implements CellFactory {
+
+    private final static String PATH = "cells.json";
+    private final static Class<Cell> TYPE = Cell.class;
+
+    private final JsonConverter<Cell> deserializer;
+
+    public CellFactoryImpl() {
+        this.deserializer = new JsonConverterImpl<>(TYPE);
+    }
+
+    @Override
+    public List<Cell> createCells() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCells'");
+    }
+
+}
