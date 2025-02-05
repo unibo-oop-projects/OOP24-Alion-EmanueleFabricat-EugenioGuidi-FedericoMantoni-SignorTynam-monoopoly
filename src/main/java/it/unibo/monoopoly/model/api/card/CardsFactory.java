cf3e65321a982c453f7ce806790c7b6a0652c31e
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-02-05 02:18:39 +0100

    create the factory interface for Cards

---
+++ src/main/java/it/unibo/monoopoly/model/api/card/CardsFactory.java
@@ -0,0 +1,14 @@
+package it.unibo.monoopoly.model.api.card;
+
+import java.util.List;
+
+/**
+ * The factory to create the cards
+ */
+public interface CardsFactory {
+
+    /**
+     * @return the {@link List} of the cards in an {@link Deck}.
+     */
+    List<Card> createDeck();
+}
