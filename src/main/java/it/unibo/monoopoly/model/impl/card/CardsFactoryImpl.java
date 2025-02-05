3521d91d50888a5771efacd4abb7c5270748b704
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-02-05 19:05:34 +0100

    change implementation of CardFactoryImpl

--- src/main/java/it/unibo/monoopoly/model/impl/card/CardsFactoryImpl.java
+++ src/main/java/it/unibo/monoopoly/model/impl/card/CardsFactoryImpl.java
@@ -1,7 +1,6 @@
 package it.unibo.monoopoly.model.impl.card;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 import it.unibo.monoopoly.model.api.card.Card;
 import it.unibo.monoopoly.model.api.card.CardsFactory;
@@ -13,9 +12,7 @@ import it.unibo.monoopoly.utils.impl.JsonConverterImpl;
  */
 public class CardsFactoryImpl implements CardsFactory {
     private final JsonConverter<Card> deserializer;
-    private static final String PATH_MOVEMENT = "cards_movement.json";
-    private static final String PATH_PAY = "cards_pay.json";
-    private static final String PATH_SPECIAL = "cards_special.json";
+    private static final String PATH = "cards.json";
 
     /* */
     public CardsFactoryImpl() {
@@ -24,19 +21,6 @@ public class CardsFactoryImpl implements CardsFactory {
 
     @Override
     public List<Card> createDeck() {
-        return mergeLists(
-            listFromPath(PATH_MOVEMENT),
-            listFromPath(PATH_PAY),
-            listFromPath(PATH_SPECIAL)
-        );
+        return this.deserializer.jsonToList(ClassLoader.getSystemResourceAsStream(PATH));
     } 
-
-    private Stream<Card> listFromPath(String path) {
-        return this.deserializer.jsonToList(ClassLoader.getSystemResourceAsStream(path)).stream();
-    }
-
-    private List<Card> mergeLists(Stream<Card> s1, Stream<Card> s2, Stream<Card> s3) {
-        return Stream.of(s1, s2, s3).flatMap(s -> s).toList();
-    }
-
 }
