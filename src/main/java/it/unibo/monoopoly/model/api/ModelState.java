8f7ce41f6a50c39fe89f59a758965dc192764168
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-02-11 22:29:53 +0100

    add unimplemented method to modelstate

--- src/main/java/it/unibo/monoopoly/model/api/ModelState.java
+++ src/main/java/it/unibo/monoopoly/model/api/ModelState.java
@@ -3,5 +3,23 @@ package it.unibo.monoopoly.model.api;
  * The model of the pattern State for the model.
  */
 public interface ModelState <X>{
-    
+    /**
+     * Set the state ready to be done.
+     */
+    void verify();
+    /**
+     * Execute the state.
+     * @param x a possible data  to elaborate.
+     */
+    void doAction(X x);
+    /**
+     * Return a data of the state.
+     * @return the data.
+     */
+    X getData();
+    /**
+     * ends the state and sets the next one.
+     */
+    void closeState();
+
 }
