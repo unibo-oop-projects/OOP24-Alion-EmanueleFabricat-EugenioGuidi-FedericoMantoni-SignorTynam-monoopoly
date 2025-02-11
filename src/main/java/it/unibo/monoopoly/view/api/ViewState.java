b365c981700005ead8aa4e07fccc2a753e51b8a4
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-02-11 22:43:36 +0100

    create the interface of view state

---
+++ src/main/java/it/unibo/monoopoly/view/api/ViewState.java
@@ -0,0 +1,7 @@
+package it.unibo.monoopoly.view.api;
+/**
+ * The model of the pattern State for the view.
+ *{@link X} a type of data that is an input, output  or both.
+ */
+public interface ViewState<X> {
+}
