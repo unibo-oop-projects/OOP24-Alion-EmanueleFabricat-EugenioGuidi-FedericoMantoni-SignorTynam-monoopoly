c7fc32625f025e04a17043ba7ed52ba5c14d1fc4
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-02-11 22:50:27 +0100

    change comment in model state

--- src/main/java/it/unibo/monoopoly/model/api/ModelState.java
+++ src/main/java/it/unibo/monoopoly/model/api/ModelState.java
@@ -1,15 +1,16 @@
 package it.unibo.monoopoly.model.api;
 /**
  * The model of the pattern State for the model.
+ *{@link X} a type of data that is an input, output  or both.
  */
 public interface ModelState <X>{
     /**
-     * Set the state ready to be done.
+     * Set the state ready to be execute.
      */
     void verify();
     /**
      * Execute the state.
-     * @param x a possible data  to elaborate.
+     * @param x a possible data to process.
      */
     void doAction(X x);
     /**
