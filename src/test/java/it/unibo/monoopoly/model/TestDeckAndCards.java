c0d3b5c51e4e7ac3aeb557a51dc44ffd4d8d537f
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-02-12 17:49:28 +0100

    start implementation of test

--- src/test/java/it/unibo/monoopoly/model/TestDeckAndCards.java
+++ src/test/java/it/unibo/monoopoly/model/TestDeckAndCards.java
@@ -1,7 +1,23 @@
 package it.unibo.monoopoly.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.monoopoly.model.api.card.Deck;
+import it.unibo.monoopoly.model.impl.card.DeckImpl;
 /**
  * Tester of {@link Deck} and {@link Card}
  */
-public class TestDeckAndCards {
+class TestDeckAndCards {
+    private Deck deck;
 
+    @BeforeEach
+    void init(){
+        this.deck = new DeckImpl();
+    }
+
+    @Test
+    void testNumberOfCards() {
+
+    }
 }
