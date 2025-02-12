7f852e4073e3ea1bbee9596cbb9c7b33a0cbd5be
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-02-12 16:57:53 +0100

    update DeckImpl with the last changes in Deck

--- src/main/java/it/unibo/monoopoly/model/impl/card/DeckImpl.java
+++ src/main/java/it/unibo/monoopoly/model/impl/card/DeckImpl.java
@@ -4,29 +4,33 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import it.unibo.monoopoly.model.api.card.Card;
 import it.unibo.monoopoly.model.api.card.CardsFactory;
 import it.unibo.monoopoly.model.api.card.Deck;
-import it.unibo.monoopoly.model.api.player.Player;
-
+import it.unibo.monoopoly.utils.Message;
+/**
+ * Implementation of {@link Deck} 
+ */
 public class DeckImpl implements Deck{
     private final CardsFactory factory = new CardsFactoryImpl();
     private List<Card> deck = new LinkedList<>();
     private Set<Card> discardPile = new HashSet<>();
     private Card actualCard;
-
+    /**
+     * Constructor that initialize and shuffle the deck
+     */
     public DeckImpl() {
         this.deck.addAll(factory.createDeck());
         shuffleDeck();
     }
     /**
-     * 
      * {@inheritDoc}
      */
     @Override
-    public void draw(Player player) {
+    public void draw() {
         if (this.deck.isEmpty()) {
             this.shuffleDeck();
         }
@@ -42,12 +46,20 @@ public class DeckImpl implements Deck{
         Collections.shuffle(this.deck);
     }
     /**
-     * 
      * {@inheritDoc}
      */
     @Override
     public Card getActualCard() {
         return this.actualCard;
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addPrisonCard() {
+         Card card = new CardImpl("Uscite gratis di prigione, se non ci siete: potete conservare questo cartoncino sino al momento di servirvene",
+                new Message(Actions.SPECIAL, Optional.of(0)));
+        this.discardPile.add(card);
     }
 
 }
