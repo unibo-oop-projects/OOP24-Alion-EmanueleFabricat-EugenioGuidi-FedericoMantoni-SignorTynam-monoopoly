31d157a85fe38be2a88d084778e97e41404a40a0
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-02-12 16:45:29 +0100

    add method addPrisonCard

--- src/main/java/it/unibo/monoopoly/model/api/card/Deck.java
+++ src/main/java/it/unibo/monoopoly/model/api/card/Deck.java
@@ -8,13 +8,15 @@ import it.unibo.monoopoly.model.api.player.Player;
  */
 public interface Deck {
     /**
-     * 
      * @param player who drawn the card. 
      */
-    void draw(Player player);
+    void draw();
     /**
-     * 
      * @return the drawn {@link Card}.
      */
     Card getActualCard();
+    /**
+     * To add a go to prison card in the discard pile.
+     */
+    void addPrisonCard();
 }
