d9934696060690294b793e1e12d90ab81681d421
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-02-13 17:34:44 +0100

    fix interface Card problem with json

--- src/main/java/it/unibo/monoopoly/model/api/card/Card.java
+++ src/main/java/it/unibo/monoopoly/model/api/card/Card.java
@@ -1,11 +1,17 @@
 package it.unibo.monoopoly.model.api.card;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 import it.unibo.monoopoly.utils.Message;
 
 /**
  * The interface that implement the resolution of draw a card.
  * The card must apply the effects itself to the player who drew the card.
  */
+@JsonTypeInfo(
+use = JsonTypeInfo.Id.CLASS,
+include = JsonTypeInfo.As.PROPERTY,
+property = "@class")
 public interface Card {
     /**
      * 
