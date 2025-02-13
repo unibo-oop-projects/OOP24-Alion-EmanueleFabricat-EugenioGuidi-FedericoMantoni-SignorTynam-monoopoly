7b085195689ac3191b74f0a1ea8d769aad70f950
Federico Mantoni <federico.mantoni@studio.unibo.it>
2025-02-13 02:21:09 +0100

    Add constructor to TurnImpl

--- src/main/java/it/unibo/monoopoly/model/impl/player/TurnImpl.java
+++ src/main/java/it/unibo/monoopoly/model/impl/player/TurnImpl.java
@@ -1,23 +1,39 @@
 package it.unibo.monoopoly.model.impl.player;
 
+import java.util.List;
 import java.util.Optional;
 
 import it.unibo.monoopoly.common.Event;
 import it.unibo.monoopoly.model.api.Notary;
+import it.unibo.monoopoly.model.api.card.Deck;
 import it.unibo.monoopoly.model.api.gameboard.Buyable;
 import it.unibo.monoopoly.model.api.gameboard.Cell;
+import it.unibo.monoopoly.model.api.gameboard.GameBoard;
 import it.unibo.monoopoly.model.api.player.Player;
 import it.unibo.monoopoly.model.api.player.Turn;
 import it.unibo.monoopoly.model.impl.NotaryImpl;
+import it.unibo.monoopoly.model.impl.card.DeckImpl;
+import it.unibo.monoopoly.model.impl.gameboard.CellFactoryImpl;
+import it.unibo.monoopoly.model.impl.gameboard.GameBoardImpl;
 
 /**
  * Implements the {@link Turn} interface.
  */
 public class TurnImpl implements Turn {
 
+    private static final int START_MONEY_AMOUNT = 1500;
+
     private Cell actualCell;
     private final Notary notary = new NotaryImpl();
     private Player actualPlayer;
+    private GameBoard gameBoard;
+    private Deck deck;
+
+    public TurnImpl(List<String> playersName) {
+        final List<Player> players = playersName.stream().map(name -> (Player) new PlayerImpl(name, START_MONEY_AMOUNT, 0)).toList();
+        this.gameBoard = new GameBoardImpl(new CellFactoryImpl().createCells(), players);
+        this.deck = new DeckImpl();
+    }
 
     /**
      * {@inheritDoc}
