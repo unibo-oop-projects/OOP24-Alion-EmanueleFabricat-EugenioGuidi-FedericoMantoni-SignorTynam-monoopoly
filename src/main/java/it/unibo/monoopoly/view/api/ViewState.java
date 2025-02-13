a883f4cb272d2800cd688b49a0442eadd96684b2
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-02-13 17:21:27 +0100

    fixed the issues with gradle build in feature/state branch

--- src/main/java/it/unibo/monoopoly/view/api/ViewState.java
+++ src/main/java/it/unibo/monoopoly/view/api/ViewState.java
@@ -1,7 +1,7 @@
 package it.unibo.monoopoly.view.api;
 /**
  * The model of the pattern State for the view.
- *{@link X}, {@link Y} types of data that are an input, output or both.
+ * @param <X>, @param <Y> types of data that are an input, output or both.
  */
 public interface ViewState<X, Y> {
     /**
