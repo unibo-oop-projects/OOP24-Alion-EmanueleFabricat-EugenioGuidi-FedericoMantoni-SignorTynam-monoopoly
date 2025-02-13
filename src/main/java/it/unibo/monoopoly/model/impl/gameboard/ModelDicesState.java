482ebd24d3e30a0d615e08a811634defe6a60ce5
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-02-13 19:38:21 +0100

    add constructor to class ModelDicesState

--- src/main/java/it/unibo/monoopoly/model/impl/gameboard/ModelDicesState.java
+++ src/main/java/it/unibo/monoopoly/model/impl/gameboard/ModelDicesState.java
@@ -1,8 +1,28 @@
 package it.unibo.monoopoly.model.impl.gameboard;
 
 import it.unibo.monoopoly.model.api.ModelState;
+import it.unibo.monoopoly.model.api.player.Player;
+import it.unibo.monoopoly.model.api.player.Turn;
+import it.unibo.monoopoly.model.api.gameboard.Dices;
 
-public class ModelDicesState implements ModelState{
+/**
+ * Implementation of {@link ModelState} for the Dices state,
+ * used to move the actual {@link Player}.
+ */
+public class ModelDicesState implements ModelState<Void, Integer>{
+
+    private final Turn turn;
+    private final Dices dices = new DicesImpl();
+
+    /**
+     * Creates a new instance of {@link ModelDicesState}.
+     * This state is used to move the current {@link Player} based on the dice result.
+     * @param turn used to associate the dices roll state with the specific 
+     * next state to execute.
+     */
+    public ModelDicesState(final Turn turn) {
+        this.turn = turn;
+    }
 
     @Override
     public void verify() {
@@ -11,13 +31,13 @@ public class ModelDicesState implements ModelState{
     }
 
     @Override
-    public void doAction(Object x) {
+    public void doAction(Void empty) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'doAction'");
     }
 
     @Override
-    public Object getData() {
+    public Integer getData() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getData'");
     }
