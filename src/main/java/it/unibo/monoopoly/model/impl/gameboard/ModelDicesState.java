eb64f28c6171b82a7f033295c107ae632bf4a032
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-02-14 00:31:42 +0100

    in class ModelDicesState implement method doAction and some new private method

--- src/main/java/it/unibo/monoopoly/model/impl/gameboard/ModelDicesState.java
+++ src/main/java/it/unibo/monoopoly/model/impl/gameboard/ModelDicesState.java
@@ -1,15 +1,20 @@
 package it.unibo.monoopoly.model.impl.gameboard;
 
+import java.util.Optional;
+
+import it.unibo.monoopoly.model.api.gameboard.Dices;
 import it.unibo.monoopoly.model.api.ModelState;
 import it.unibo.monoopoly.model.api.player.Player;
 import it.unibo.monoopoly.model.api.player.Turn;
-import it.unibo.monoopoly.model.api.gameboard.Dices;
+import it.unibo.monoopoly.model.api.gameboard.Dices.Pair;
 
 /**
  * Implementation of {@link ModelState} for the Dices state,
  * used to move the actual {@link Player}.
  */
-public class ModelDicesState implements ModelState<Void, Integer>{
+public class ModelDicesState implements ModelState<Optional<Integer>, Pair>{
+
+    private static final int PASS_GO_REWARD = 200;
 
     private final Turn turn;
     private final Dices dices = new DicesImpl();
@@ -24,20 +29,43 @@ public class ModelDicesState implements ModelState<Void, Integer>{
         this.turn = turn;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void verify() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'verify'");
+    public void verify() {}
+
+    @Override
+    public void doAction(final Optional<Integer> cellIndex) {
+        if(cellIndex.isEmpty()) {
+            this.dices.rollDices();
+            if(checkPassGo()) {
+                this.turn.getActualPlayer().receive(PASS_GO_REWARD);
+                movePlayer((getPlayerPosition() + this.dices.getResult()) % this.turn.getCellsList().size());
+            } else {
+                movePlayer(getPlayerPosition() + this.dices.getResult());
+            }
+        } else {
+            if(this.turn.getActualPlayer().isPrisoned()) {
+                movePlayer(cellIndex.get());
+            }
+        }
+    }
+
+    private int getPlayerPosition() {
+        return this.turn.getActualPlayer().getActualPosition();
+    }
+
+    private void movePlayer(final int cellIndex) {
+        this.turn.getActualPlayer().changePosition(cellIndex);
+    }
+
+    private boolean checkPassGo() {
+        return getPlayerPosition() + this.dices.getResult() >= this.turn.getCellsList().size();
     }
 
     @Override
-    public void doAction(Void empty) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'doAction'");
-    }
-
-    @Override
-    public Integer getData() {
+    public Pair getData() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getData'");
     }
