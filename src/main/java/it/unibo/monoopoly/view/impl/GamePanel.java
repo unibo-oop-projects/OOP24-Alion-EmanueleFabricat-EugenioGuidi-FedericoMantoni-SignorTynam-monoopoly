4dc6048eda52b6b5cd9c9b23001ae0e5c7230bfa
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-02-14 17:54:16 +0100

    create class GamePanel that extends PanelAdapter

---
+++ src/main/java/it/unibo/monoopoly/view/impl/GamePanel.java
@@ -0,0 +1,19 @@
+package it.unibo.monoopoly.view.impl;
+
+import java.awt.Dimension;
+
+public class GamePanel extends PanelAdapter{
+
+    @Override
+    public void resizeText(Dimension screenSize) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resizeText'");
+    }
+
+    @Override
+    protected void panelInit() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'panelInit'");
+    }
+
+}
