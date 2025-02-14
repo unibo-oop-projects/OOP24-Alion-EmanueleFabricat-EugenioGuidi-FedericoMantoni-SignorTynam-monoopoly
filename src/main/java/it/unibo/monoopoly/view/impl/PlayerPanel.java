f948c06d4c2eb6e6efb69580ee21764eb3600ea9
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-02-14 22:47:53 +0100

    add class PlayerImpl

--- src/main/java/it/unibo/monoopoly/view/impl/PlayerPanel.java
+++ src/main/java/it/unibo/monoopoly/view/impl/PlayerPanel.java
@@ -1,5 +1,19 @@
 package it.unibo.monoopoly.view.impl;
 
-public class PlayerPanel {
+import java.awt.Dimension;
+
+public class PlayerPanel extends PanelAdapter{
+
+    @Override
+    public void resizeText(Dimension screenSize) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resizeText'");
+    }
+
+    @Override
+    protected void panelInit() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'panelInit'");
+    }
 
 }
