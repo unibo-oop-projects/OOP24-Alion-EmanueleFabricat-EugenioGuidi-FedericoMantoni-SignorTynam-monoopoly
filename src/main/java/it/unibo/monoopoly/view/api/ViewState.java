52da6adb63455583a711f8f9f33ae5637bcb234e
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-02-16 22:11:29 +0100

    fix ViewState comment

--- src/main/java/it/unibo/monoopoly/view/api/ViewState.java
+++ src/main/java/it/unibo/monoopoly/view/api/ViewState.java
@@ -5,12 +5,11 @@ package it.unibo.monoopoly.view.api;
 public interface ViewState {
     /**
      * Set the state ready to be execute.
-     * @param x a possible data to help the setup.
+     * @param set a possible data to help the setup.
      */
     void setMode(boolean set);
     /**
      * Display all the views necessary for the state.
-     * @param y a possible data to display.
      */
     void visualize();
 
