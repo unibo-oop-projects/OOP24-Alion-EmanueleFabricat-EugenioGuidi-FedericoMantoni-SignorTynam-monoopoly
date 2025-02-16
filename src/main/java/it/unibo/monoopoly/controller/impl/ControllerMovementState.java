820698eb6602441e5aff826cdaec1dca0e61ab53
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-02-16 16:18:28 +0100

    add new class ControllerMovementState that implements ControllerState

---
+++ src/main/java/it/unibo/monoopoly/controller/impl/ControllerMovementState.java
@@ -0,0 +1,19 @@
+package it.unibo.monoopoly.controller.impl;
+
+import it.unibo.monoopoly.controller.api.ControllerState;
+
+public class ControllerMovementState implements ControllerState{
+
+    @Override
+    public void startState() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'startState'");
+    }
+
+    @Override
+    public void continueState(Object x) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'continueState'");
+    }
+
+}
