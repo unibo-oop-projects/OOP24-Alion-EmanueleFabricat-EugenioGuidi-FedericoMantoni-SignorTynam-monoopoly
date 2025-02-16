ed9b1996d5895b7f7d9d705fa2e60e3b7b758c88
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-02-16 11:53:11 +0100

    add image of monoopoly gameboard in class GameBoardPanel

--- src/main/java/it/unibo/monoopoly/view/impl/GameBoardPanel.java
+++ src/main/java/it/unibo/monoopoly/view/impl/GameBoardPanel.java
@@ -2,6 +2,8 @@ package it.unibo.monoopoly.view.impl;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Image;
+import java.net.URL;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -22,11 +24,22 @@ public class GameBoardPanel extends PanelAdapter{
 
     @Override
     protected void panelInit() {
+        setLayout(null);
 
-        /*final URL imgURL = ClassLoader.getSystemResource("/images/gameBoard.jpg");
-        final ImageIcon icon = new ImageIcon(imgURL);
-        JLabel gameBoardLabel = new JLabel(icon);
-        add(gameBoardLabel);*/
+        final URL imgURL = ClassLoader.getSystemResource("images/MONOOPOLY_GAMEBOARD_IMAGE.jpg");
+        ImageIcon icon = new ImageIcon(imgURL);
+        Image image = icon.getImage();
+        Image scaledImage = image.getScaledInstance(this.mainFrameWidth, this.mainFrameWidth, image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+
+        JLabel gameBoardLabel = new JLabel(scaledIcon);
+        gameBoardLabel.setHorizontalAlignment(JLabel.CENTER);
+        gameBoardLabel.setVerticalAlignment(JLabel.CENTER);
+        gameBoardLabel.setBorder(null);
+        gameBoardLabel.setText("");   
+        gameBoardLabel.setBounds(0, 0, this.mainFrameWidth, this.mainFrameWidth);
+
+        add(gameBoardLabel);
         setBackground(Color.RED);
         add(new JLabel("MONOPOLY"));
     }
