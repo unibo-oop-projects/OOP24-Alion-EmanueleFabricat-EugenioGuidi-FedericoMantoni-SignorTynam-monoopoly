b4286ffc82c1d39167d4201c2ba19a75529291d7
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-02-16 17:49:48 +0100

    create class ViewMovementState that implements VIewState interface

---
+++ src/main/java/it/unibo/monoopoly/view/impl/ViewMovementState.java
@@ -0,0 +1,20 @@
+package it.unibo.monoopoly.view.impl;
+
+import it.unibo.monoopoly.view.api.ViewState;
+import it.unibo.monoopoly.model.api.gameboard.Dices.Pair;
+
+public class ViewMovementState implements ViewState<Boolean, Pair> {
+
+    @Override
+    public void setMode(Boolean x) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setMode'");
+    }
+
+    @Override
+    public void visualize(Pair y) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visualize'");
+    }
+
+}
