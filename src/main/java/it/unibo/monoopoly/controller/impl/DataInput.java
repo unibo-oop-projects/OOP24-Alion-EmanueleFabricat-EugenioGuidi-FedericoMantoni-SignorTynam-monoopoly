43842e050976322b6ae9546c1513ab409b20fb65
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-02-16 21:54:15 +0100

    create a record DataInput

---
+++ src/main/java/it/unibo/monoopoly/controller/impl/DataInput.java
@@ -0,0 +1,18 @@
+package it.unibo.monoopoly.controller.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import it.unibo.monoopoly.common.Event;
+import it.unibo.monoopoly.model.api.gameboard.Dices.Pair;
+
+public record DataInput(
+    Optional<Event> event,
+    Optional<String> nameOfProperty,
+    Optional<String> nameOfPlayer,
+    Optional<Pair> dices,
+    Optional<Boolean> setMode1,
+    Optional<Boolean> setMode2,
+    Optional<List<Integer>> cellList
+) {
+}
