94be81902b250d0ae469496915f32d40cc5afbc0
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-02-16 17:54:17 +0100

    in class ViewMovementState add constructor

--- src/main/java/it/unibo/monoopoly/view/impl/ViewMovementState.java
+++ src/main/java/it/unibo/monoopoly/view/impl/ViewMovementState.java
@@ -5,6 +5,13 @@ import it.unibo.monoopoly.model.api.gameboard.Dices.Pair;
 
 public class ViewMovementState implements ViewState<Boolean, Pair> {
 
+    private final MainView mainVIew;
+    private boolean showRollDice;
+
+    public ViewMovementState(final MainView mainView) {
+        this.mainVIew = mainView;
+    }
+
     @Override
     public void setMode(Boolean x) {
         // TODO Auto-generated method stub
