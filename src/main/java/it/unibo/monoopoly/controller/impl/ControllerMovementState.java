7a2e96deaa16d3410114bd67f75bf180865681aa
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-02-16 17:41:35 +0100

    in class ControllerMovementState implement method startState

--- src/main/java/it/unibo/monoopoly/controller/impl/ControllerMovementState.java
+++ src/main/java/it/unibo/monoopoly/controller/impl/ControllerMovementState.java
@@ -1,20 +1,31 @@
 package it.unibo.monoopoly.controller.impl;
 
+import java.util.List;
+import java.util.Optional;
+
+import it.unibo.monoopoly.model.api.gameboard.Dices.Pair;
 import it.unibo.monoopoly.controller.api.ControllerState;
 import it.unibo.monoopoly.controller.api.MainController;
+import it.unibo.monoopoly.model.api.ModelState;
+import it.unibo.monoopoly.view.api.ViewState;
 
 public class ControllerMovementState implements ControllerState{
 
-    private MainController mainController;
+    private final MainController mainController;
+    private final ModelState<Void, Pair> actualModelState;
+    private final ViewState<Boolean, Pair> actualViewState;
 
     public ControllerMovementState(final MainController mainController) {
         this.mainController = mainController;
+        this.actualModelState = mainController.getModelState();
+        this.actualViewState = this.mainController.getViewState();
     }
 
     @Override
     public void startState() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'startState'");
+        this.actualViewState.setMode(this.actualModelState.verify());//ritorna se è con dadi o no
+        this.actualModelState.doAction(null);
+        this.actualViewState.visualize(this.actualModelState.getData());
     }
 
     @Override
