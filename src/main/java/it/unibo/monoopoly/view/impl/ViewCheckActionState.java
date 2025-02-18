8ec6fd3ef99cd04da07bc2fecc05b4f53ddf350b
Federico Mantoni <federico.mantoni@studio.unibo.it>
2025-02-18 21:26:11 +0100

    modify ViewCheckActionState to fit to the new classes BuilderData

--- src/main/java/it/unibo/monoopoly/view/impl/ViewCheckActionState.java
+++ src/main/java/it/unibo/monoopoly/view/impl/ViewCheckActionState.java
@@ -1,5 +1,6 @@
 package it.unibo.monoopoly.view.impl;
 
+import java.lang.foreign.Linker.Option;
 import java.util.Optional;
 
 import javax.swing.JOptionPane;
@@ -13,43 +14,48 @@ public class ViewCheckActionState implements ViewState {
 
     private static final String[] YES_NO = {"Sì", "No"};
     private final View mainView;
-    private DataInput dataInput;
+    private Optional<DataInput> dataInput;
 
     public ViewCheckActionState(final View mainView) {
         this.mainView = mainView;
+        this.dataInput = Optional.empty();
     }
 
     @Override
     public void setMode(DataInput dataInput) {
-        this.dataInput = dataInput;
+        this.dataInput = Optional.of(dataInput);
     }
 
     @Override
     public void visualize() {
-        switch (this.dataInput.event().get()) {
-            case RENT_PAYMENT -> JOptionPane.showMessageDialog(mainView.getMainFrame(),
-            "Devi pagare " + this.dataInput.valueToPay().get() + "€ a " + this.dataInput.nameOfPlayer().get(),
-            "Pagamento affitto", JOptionPane.PLAIN_MESSAGE);
-
-            case TAX_PAYMENT -> JOptionPane.showMessageDialog(mainView.getMainFrame(),
-            "Devi pagare " + this.dataInput.valueToPay().get() + "€ a " + this.dataInput.nameOfPlayer().get(), "Pagamento tassa",
-            JOptionPane.PLAIN_MESSAGE);
-
-            case BUY_PROPERTY -> {
-            final int choice = JOptionPane.showOptionDialog(mainView.getMainFrame(),
-            "Vuoi comprare la proprietà " + this.dataInput.nameOfProperty().get() + "al costo di " + this.dataInput.valueToPay().get() + "€",
-            "Compra proprietà",
-            JOptionPane.YES_NO_OPTION,
-            JOptionPane.QUESTION_MESSAGE, null,
-            YES_NO, 1);
-            if(choice == 0) {
-                mainView.getMainController().getControllerState().continueState(new DataOutput(Optional.of(true), Optional.empty()));
-            } else {
-                mainView.getMainController().getControllerState().continueState(new DataOutput(Optional.of(false), Optional.empty()));
+        if(dataInput.isPresent()) {
+            final DataInput input = this.dataInput.get();
+            switch (input.event().get()) {
+                case RENT_PAYMENT -> JOptionPane.showMessageDialog(mainView.getMainFrame(),
+                "Devi pagare " + input.valueToPay().get() + "€ a " + input.nameOfPlayer().get(),
+                "Pagamento affitto", JOptionPane.PLAIN_MESSAGE);
+    
+                case TAX_PAYMENT -> JOptionPane.showMessageDialog(mainView.getMainFrame(),
+                "Devi pagare " + input.valueToPay().get() + "€ a " + input.nameOfPlayer().get(), "Pagamento tassa",
+                JOptionPane.PLAIN_MESSAGE);
+    
+                case BUY_PROPERTY -> {
+                final int choice = JOptionPane.showOptionDialog(mainView.getMainFrame(),
+                "Vuoi comprare la proprietà " + input.nameOfProperty().get() + "al costo di " + input.valueToPay().get() + "€",
+                "Compra proprietà",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE, null,
+                YES_NO, 1);
+                if(choice == 0) {
+                    mainView.getMainController().getControllerState().continueState(new DataOutput(Optional.of(true), Optional.empty()));
+                } else {
+                    mainView.getMainController().getControllerState().continueState(new DataOutput(Optional.of(false), Optional.empty()));
+                }
+            }
+                default -> throw new IllegalArgumentException("Nothing to visualize in this state");
             }
         }
-            default -> throw new IllegalArgumentException("Nothing to visualize in this state");
-        }
+        
     }
 
 }
