0168b72feba32ce38bf618d7372014fc29cfd7fc
Federico Mantoni <federico.mantoni@studio.unibo.it>
2025-02-18 21:42:03 +0100

    change constructor of ControllerCheckActionState

--- src/main/java/it/unibo/monoopoly/controller/impl/ControllerCheckActionState.java
+++ src/main/java/it/unibo/monoopoly/controller/impl/ControllerCheckActionState.java
@@ -19,10 +19,11 @@ public class ControllerCheckActionState implements ControllerState {
     private final Optional<Event> actualEvent;
     private final Cell actualCell;
 
-    public ControllerCheckActionState (MainController mainController, Optional<Event> event, Cell actualCell) {
+    public ControllerCheckActionState(MainController mainController, ModelState modelState,
+            ViewState viewState, Optional<Event> event, Cell actualCell) {
         this.mainController = mainController;
-        this.modelState = this.mainController.getModelState();
-        this.viewState = this.mainController.getViewState();
+        this.modelState = modelState;
+        this.viewState = viewState;
         this.actualEvent = event;
         this.actualCell = actualCell;
     }
@@ -31,19 +32,19 @@ public class ControllerCheckActionState implements ControllerState {
     public void startState() {
         if (modelState.verify()) {
             viewState.setMode(new DataBuilderInputImpl()
-                .event(Event.BUY_PROPERTY)
-                .valueToPay(((Buyable) actualCell).getCost())
-                .nameOfProperty(actualCell.getName()).build());
+                    .event(Event.BUY_PROPERTY)
+                    .valueToPay(((Buyable) actualCell).getCost())
+                    .nameOfProperty(actualCell.getName()).build());
         } else if (actualEvent.isPresent()) {
             modelState.doAction(Optional.empty());
             if (actualEvent.get().equals(Event.RENT_PAYMENT)) {
                 viewState.setMode(new DataBuilderInputImpl().event(actualEvent.get())
-                    .valueToPay(((Buyable) actualCell).getRentalValue())
-                    .nameOfPlayer(((Buyable) actualCell).getOwner().get().getName()).build());
+                        .valueToPay(((Buyable) actualCell).getRentalValue())
+                        .nameOfPlayer(((Buyable) actualCell).getOwner().get().getName()).build());
             } else if (actualEvent.get().equals(Event.TAX_PAYMENT)) {
                 viewState.setMode(new DataBuilderInputImpl()
-                .event(actualEvent.get())
-                .valueToPay(((Functional) actualCell).getAction().get().data().get()).build());
+                        .event(actualEvent.get())
+                        .valueToPay(((Functional) actualCell).getAction().get().data().get()).build());
             }
         }
         viewState.visualize();
@@ -52,7 +53,7 @@ public class ControllerCheckActionState implements ControllerState {
 
     @Override
     public void continueState(DataOutput dataOutput) {
-        if(dataOutput.buyProperty().isPresent()) {
+        if (dataOutput.buyProperty().isPresent()) {
             modelState.doAction(Optional.of(dataOutput));
         }
         modelState.closeState();
