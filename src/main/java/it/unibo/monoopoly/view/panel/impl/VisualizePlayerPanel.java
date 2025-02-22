14dc4eb8f5f51af2cf0d3f6ff65523e85ad6d92a
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-02-22 23:57:26 +0100

    fix VisualizePlayerPanel

--- src/main/java/it/unibo/monoopoly/view/panel/impl/VisualizePlayerPanel.java
+++ src/main/java/it/unibo/monoopoly/view/panel/impl/VisualizePlayerPanel.java
@@ -5,11 +5,13 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
+import javax.swing.border.LineBorder;
 
 import org.apache.commons.lang3.tuple.Triple;
 
@@ -19,8 +21,7 @@ public class VisualizePlayerPanel extends PanelAdapter {
     private final int mainFrameHeight;
     private final String firstPlayer;
     private final List<Triple<String, Integer, Color>> initializedList;
-    private final JTextArea name = new JTextArea();
-    private final JTextArea amount = new JTextArea();
+    private final List<JTextArea> textList = new LinkedList<>();
     private static final double PERC_RESIZE = 0.035; 
 
 
@@ -34,29 +35,35 @@ public class VisualizePlayerPanel extends PanelAdapter {
 
     @Override
     protected void panelInit() {
-        setLayout(new GridLayout(playersNumber * 2, 1));
-        this.name.setEnabled(false);
-        this.amount.setEnabled(false);
-        this.name.setFont(new Font("Arial", Font.PLAIN, (int) (PERC_RESIZE * this.mainFrameHeight)));
-        this.amount.setFont(new Font("Arial", Font.PLAIN, (int) (PERC_RESIZE * this.mainFrameHeight)));
+        setLayout(new GridLayout(playersNumber * 2 + 1, 1));
+        this.textList.add(new JTextArea("E' il turno di " + this.firstPlayer));
         for (Triple<String,Integer,Color> triple : initializedList) {
-            name.setText(triple.getLeft());
-            add(name);
-            add(amount);
+            this.textList.add(new JTextArea(triple.getLeft()));
+            this.textList.getLast().setBackground(triple.getRight());
+            this.textList.add(new JTextArea(String.valueOf(triple.getMiddle()) + " €"));
+            this.textList.getLast().setBackground(triple.getRight());            
+        }
+        for (var text : this.textList) {
+            text.setEnabled(false);
+            text.setFont(new Font("Arial", Font.PLAIN, (int) (0.035 * this.mainFrameHeight)));
+            add(text);
         }
     }
 
-    /*private void update(final String actualPlayer, final Map<String, Integer> playersCredit,
-            final Map<Color, String> playersColors) {
-        for (final var entry : playersCredit.entrySet()) {
-            final JTextArea text = new JTextArea(entry.getKey() + " \n\nPatrimonio: " + entry.getValue() + " €");
-            playersColors.entrySet().stream()
-                    .filter(e -> e.getValue().equals(entry.getKey()))
-                    .map(Map.Entry::getKey)
-                    .findFirst()
-                    .ifPresent(text::setBackground);
-            text.setFont(new Font("Arial", Font.PLAIN, (int) (0.035 * this.mainFrameHeight)));
-            text.setEnabled(false);
-            add(text);
-        }*/
+    public void update(final String actualPlayer, final List<Integer> amounts) {
+        this.textList.get(0).setText("E' il turno di " + actualPlayer);
+        int i = 1;
+        for (; i < amounts.size(); i++) {
+            this.textList.get(i + 1).setText(String.valueOf(amounts.get(i)) + " €");
+        }
+        if (this.textList.size() > amounts.size()) {
+            for (; i < this.textList.size(); i++) {
+                this.textList.get(i).setBackground(Color.GRAY);
+                if (i % 2 == 0) {
+                    this.textList.get(i).setText("BANCAROTTA");
+                }
+            }
+        }
     }
+}
+
