42a7eba642fb3e8aa099850833fa45eddcdedffc
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-02-22 14:09:16 +0100

    in class PositionsFactoryImpl add constructor

--- src/main/java/it/unibo/monoopoly/view/panel/impl/PositionsFactoryImpl.java
+++ src/main/java/it/unibo/monoopoly/view/panel/impl/PositionsFactoryImpl.java
@@ -4,8 +4,20 @@ import java.util.List;
 
 import it.unibo.monoopoly.view.panel.api.PositionsFactory;
 import it.unibo.monoopoly.view.panel.impl.GameBoardPanel.Position;
+import it.unibo.monoopoly.model.deck.api.Card;
+import it.unibo.monoopoly.utils.api.JsonConverter;
+import it.unibo.monoopoly.utils.impl.JsonConverterImpl;
 
 public class PositionsFactoryImpl implements PositionsFactory{
+
+    private final JsonConverter<Position> converter;
+    private final String fileName;
+
+
+    public PositionsFactoryImpl(final String fileName) {
+        this.fileName = fileName;
+        this.converter = new JsonConverterImpl<>(Position.class);
+    }
 
     @Override
     public List<Position> createPositions() {
