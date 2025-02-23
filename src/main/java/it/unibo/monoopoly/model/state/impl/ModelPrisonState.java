dfbc3724e69f30f59efc702f28fc5ef297373841
alion <alion.emini2@studio.unibo.it>
2025-02-23 23:38:12 +0100

    refactor ModelPrisonState to implement game logic and remove unused code

--- src/main/java/it/unibo/monoopoly/model/state/impl/ModelPrisonState.java
+++ src/main/java/it/unibo/monoopoly/model/state/impl/ModelPrisonState.java
@@ -7,95 +7,48 @@ import it.unibo.monoopoly.model.main.api.MainModel;
 import it.unibo.monoopoly.model.player.api.Player;
 import it.unibo.monoopoly.model.state.api.ModelState;
 
-/**
- * comment.
- */
 public class ModelPrisonState implements ModelState {
-    /**
-     *
-     * {@inheritDoc}
-     */
+
+    private final MainModel model;
+    private final boolean goInJail;
+    private boolean usedCard;
+
+    public ModelPrisonState(MainModel model, boolean goInJail) {
+        this.model = model;
+        this.goInJail = goInJail;
+        this.usedCard = false;
+    }
+
     @Override
     public boolean verify() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'verify'");
+        return goInJail;
     }
 
-    /**
-     *
-     * {@inheritDoc}
-     */
     @Override
-    public void doAction(final DataOutput data) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'doAction'");
+    public void doAction(DataOutput data) {
+        Player currentPlayer = model.getGameBoard().getCurrentPlayer();
+        if (goInJail) {
+            currentPlayer.setPrisoned();
+        } else {
+            if (currentPlayer.getFreeJailCards() > 0) {
+                usedCard = true;
+                currentPlayer.useGetOutOfJailCard();
+                model.getGameBoard().getDeck().addPrisonCard();
+            }
+            currentPlayer.releaseFromPrison();
+        }
     }
 
-    /**
-     *
-     * {@inheritDoc}
-     */
     @Override
     public void closeState() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'closeState'");
+        if (goInJail) {
+            model.nextTurn();
+        } else {
+            if (usedCard) {
+                model.setState(new ModelMovementState(model, Optional.empty()));
+            } else {
+                model.setState(new ModelBankerState(model, 50, true));
+            }
+        }
     }
-    /*
-     * private final Turn mainModel;
-     * private String message;
-     * 
-     * public ModelPrisonState(Turn mainModel, boolean goToPrison) {
-     * this.mainModel = mainModel;
-     * 
-     * }
-     * 
-     * @Override
-     * public boolean verify() {
-     * if (player.isPrisoned()) {
-     * message = "You are in prison, you can't move";
-     * return true;
-     * } else {
-     * message = "You are not in prison, you can move";
-     * return false;
-     * }
-     * }
-     * 
-     * @Override
-     * public void doAction(Boolean useCard) {
-     * if (!player.isPrisoned()) {
-     * message = "You are not in prison, you can move";
-     * return;
-     * }
-     * if (useCard != null && useCard) {
-     * if (player.getFreeJailCards() > 0) {
-     * boolean used = player.useGetOutOfJailCard();
-     * if (used) {
-     * message = "You used a get out of jail card";
-     * } else {
-     * message = "You don't have any get out of jail card";
-     * }
-     * } else {
-     * message = "Cannot use a get out of jail card, you don't have any";
-     * }
-     * } else {
-     * if (player.isPayable(50)) {
-     * player.pay(50);
-     * message = "You paid 50 and you are out of prison";
-     * player.releaseFromPrison();
-     * } else {
-     * message = "You can't afford to pay, you risk bankruptcy.";
-     * }
-     * }
-     * }
-     * 
-     * @Override
-     * public String getData() {
-     * return message;
-     * }
-     * 
-     * @Override
-     * public void closeState() {
-     * 
-     * }
-     */
 }
