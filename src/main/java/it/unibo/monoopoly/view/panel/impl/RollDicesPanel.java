33e4dbbcd00e07713292a20b200912cb8f4da470
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-03-01 20:45:47 +0100

    create new class RollDIcesPanel

---
+++ src/main/java/it/unibo/monoopoly/view/panel/impl/RollDicesPanel.java
@@ -0,0 +1,17 @@
+package it.unibo.monoopoly.view.panel.impl;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class RollDicesPanel extends JPanel{
+    public RollDicesPanel(final ActionListener actionListener) {
+        final JButton rollDiceButton = new JButton("Lancia dadi");
+        rollDiceButton.addActionListener(actionListener);
+        this.setLayout(new BorderLayout());
+        this.add(rollDiceButton, BorderLayout.CENTER);
+        
+    }
+}
