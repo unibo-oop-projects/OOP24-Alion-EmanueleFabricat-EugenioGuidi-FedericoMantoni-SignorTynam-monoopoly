6613cb481e8ec4c62cbb08e7c32a3e134af91f6d
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-03-02 20:35:13 +0100

    in class GameBoardPanel remove useless methods

--- src/main/java/it/unibo/monoopoly/view/panel/api/PositionAllocator.java
+++ src/main/java/it/unibo/monoopoly/view/panel/api/PositionAllocator.java
@@ -1,5 +1,15 @@
 package it.unibo.monoopoly.view.panel.api;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import it.unibo.monoopoly.view.panel.impl.NumberAndCirclePosition;
+
 public interface PositionAllocator {
 
+    List<NumberAndCirclePosition> createListCircleNumberPosition(Map<String, Integer> newPlayersPositions, 
+                                                                 Map<Integer, Optional<String>> cellsOwners, 
+                                                                 List<String> prisonedPlayers,
+                                                                 Map<Integer, Integer> nBuiltHouses);
 }
--- src/main/java/it/unibo/monoopoly/view/panel/impl/GameBoardPanel.java
+++ src/main/java/it/unibo/monoopoly/view/panel/impl/GameBoardPanel.java
@@ -22,6 +22,7 @@ import javax.swing.JPanel;
 import it.unibo.monoopoly.utils.api.PositionsFactory;
 import it.unibo.monoopoly.utils.impl.Position;
 import it.unibo.monoopoly.utils.impl.PositionsFactoryImpl;
+import it.unibo.monoopoly.view.panel.api.PositionAllocator;
 
 /**
  * mettere commento qui, tutti i numeri molotiplicativi vengono percepiti come
@@ -32,6 +33,7 @@ public final class GameBoardPanel extends JPanel {
     private final int mainFrameHeight;
     private final Image backgroundImage;
     private List<NumberAndCirclePosition> numberAndCirclePositions;
+    private final PositionAllocator positionAllocator;
 
     /**
      * 
@@ -41,78 +43,22 @@ public final class GameBoardPanel extends JPanel {
      */
     public GameBoardPanel(final int mainFrameHeight, final Map<Color, String> players, final List<Color> colors) {
         this.mainFrameHeight = mainFrameHeight;
+        this.positionAllocator = new PositionAllocatorImpl(mainFrameHeight, players, colors);
 
         final URL imgURL = ClassLoader.getSystemResource("images/monoopoly_gameboard_image.jpg");
         final ImageIcon icon = new ImageIcon(imgURL);
         this.backgroundImage = icon.getImage();
-        this.circlesPositions = new ArrayList<>();
         setPreferredSize(new Dimension(this.mainFrameHeight, this.mainFrameHeight));
         setLayout(new BorderLayout());
     }
 
     public void update(Map<String, Integer> newPlayersPositions, Map<Integer, Optional<String>> cellsOwners,
                        Map<Integer, Integer> nBuiltHouses, List<String> prisonedPlayers) {
-        this.circlesPositions = initializeCirclesPositions(newPlayersPositions, cellsOwners, prisonedPlayers);
-        this.numberPositions = initializeNumbersPositions(nBuiltHouses);
+        this.numberAndCirclePositions = this.positionAllocator.createListCircleNumberPosition(newPlayersPositions, 
+                                                                                              cellsOwners, 
+                                                                                              prisonedPlayers,
+                                                                                              nBuiltHouses);
         this.repaint();
-    }
-
-    private List<NumberPosition> initializeNumbersPositions(Map<Integer, Integer> nBuiltHouses) {
-        final List<NumberPosition> newList = new ArrayList<>();
-        for(var entry : nBuiltHouses.entrySet()) {
-            NumberPosition numberPosition = new NumberPosition(this.housesPositions.get(entry.getKey()).x(), 
-                                                               this.housesPositions.get(entry.getKey()).y(), 
-                                                               entry.getValue().toString());
-            newList.add(numberPosition);
-        }
-
-        return newList;
-    }
-
-    private List<CirclePosition> initializeCirclesPositions(Map<String, Integer> newPlayersPositions, 
-                                                           Map<Integer, Optional<String>> cellsOwners, 
-                                                           List<String> prisonedPlayers) {
-        final List<CirclePosition> newList = new ArrayList<>();
-        for(var entry : this.playersColors.entrySet()) {
-            if(newPlayersPositions.containsKey(entry.getValue())){
-                CirclePosition circlePosition = new CirclePosition(getX(entry, newPlayersPositions), 
-                                                                   getY(entry, newPlayersPositions), 
-                                                                   entry.getKey());
-                newList.add(circlePosition);
-            }
-        }
-        for(var entry : cellsOwners.entrySet()) {
-            if(entry.getValue().isPresent()) {
-                CirclePosition circlePosition = new CirclePosition(this.propertyPositions.get(entry.getKey()).x(),
-                                                                   this.propertyPositions.get(entry.getKey()).y(),
-                                                                   getColorFromString(entry.getValue().get()));
-                newList.add(circlePosition);
-            }
-        }
-        for(var prisonedPlayer : prisonedPlayers) {
-            Color color = getColorFromString(prisonedPlayer);
-            CirclePosition circlePosition = new CirclePosition(this.prisonPositions.get(color).x(), 
-                                                               this.prisonPositions.get(color).y(), 
-                                                               color);
-            newList.add(circlePosition);
-        }
-        return newList;
-    }
-
-    private Color getColorFromString(final String player) {
-        return playersColors.entrySet().stream()
-                                       .filter(entry -> entry.getValue().equals(player))
-                                       .map(Map.Entry::getKey)
-                                       .findFirst()
-                                       .orElse(null);
-    }
-
-    private double getX(final Map.Entry<Color, String> entry, final Map<String, Integer> newPlayersPositions) {
-        return this.playersPositions.get(entry.getKey()).get(newPlayersPositions.get(entry.getValue())).x();
-    }
-
-    private double getY(final Map.Entry<Color, String> entry, final Map<String, Integer> newPlayersPositions) {
-        return this.playersPositions.get(entry.getKey()).get(newPlayersPositions.get(entry.getValue())).y();
     }
 
     /**
