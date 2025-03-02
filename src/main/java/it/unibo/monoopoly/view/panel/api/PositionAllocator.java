e1309964f992bdb0fa0bdea37eb7bbbaa73dc36c
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2025-03-02 17:11:41 +0100

    add interface PositionAllocator and class PositionAllocatorImpl

---
+++ src/main/java/it/unibo/monoopoly/view/panel/api/PositionAllocator.java
@@ -0,0 +1,5 @@
+package it.unibo.monoopoly.view.panel.api;
+
+public interface PositionAllocator {
+
+}
---
+++ src/main/java/it/unibo/monoopoly/view/panel/impl/PositionAllocatorImpl.java
@@ -0,0 +1,7 @@
+package it.unibo.monoopoly.view.panel.impl;
+
+import it.unibo.monoopoly.view.panel.api.PositionAllocator;
+
+public class PositionAllocatorImpl implements PositionAllocator{
+    
+}
