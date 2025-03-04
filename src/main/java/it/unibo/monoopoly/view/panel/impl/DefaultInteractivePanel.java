fc4878cf3837e04ff48d00466bf0a3d1ceb6667a
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-03-04 17:52:02 +0100

    change a text in DefoultInteractivePanel

--- src/main/java/it/unibo/monoopoly/view/panel/impl/DefaultInteractivePanel.java
+++ src/main/java/it/unibo/monoopoly/view/panel/impl/DefaultInteractivePanel.java
@@ -12,7 +12,16 @@ public final class DefaultInteractivePanel extends JPanel {
     private static final long serialVersionUID = 1L;
 
     public DefaultInteractivePanel() {
-        final JTextArea startText = new JTextArea("Qui appariranno le celle di propria proprietà tra cui scegliere.\nSe si vuole chiudere l'applicazione:\ncontinuare il gioco fino ad arrivare a una di quelle scelte, poi premere esc e selezionare si");
+        final JTextArea startText = new JTextArea(
+                "Qui appariranno le celle di propria proprietà tra cui scegliere.\n" + 
+                "Se si vuole chiudere l'applicazione:\n" + 
+                "continuare il gioco fino ad arrivare a una di quelle scelte, poi premere esc e selezionare si\n" + 
+                "Ora una piccola spiegazione della gameBoard:\n" +
+                "Lo spostamento di un giocatore è visualizzato tramite lo spostamento del pallino col relativo colore nelle celle\n" +
+                "Se una proprietà appartiene a un giocatore sarà visibile un pallino del colore corrispondente al di sopra\n" +
+                "Se un proprietà è in bancarotta sarà visibile un pallino grigio al di sopra" +
+                "Nel momento in cui si costruirà una casa su una proprietà comparira un numero sulla relativa banda colorata a identificare il numero di case costruite\n" +
+                "Infine i messaggi di notifica o le scelte semplici compariranno in una finestrella al centro del pannello");
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         startText.setFont(new Font("Arial", Font.PLAIN, FONT_SIZE));
         startText.setEnabled(false);
