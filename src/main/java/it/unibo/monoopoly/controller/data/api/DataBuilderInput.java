248e4f5389f8b3d2cee6f52a93d60f6a7e965f24
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-03-06 19:08:57 +0100

    comment DataBuilderInput

--- src/main/java/it/unibo/monoopoly/controller/data/api/DataBuilderInput.java
+++ src/main/java/it/unibo/monoopoly/controller/data/api/DataBuilderInput.java
@@ -7,53 +7,53 @@ import it.unibo.monoopoly.controller.data.impl.DataInput;
 import it.unibo.monoopoly.model.gameboard.api.Dices.Pair;
 
 /**
- * The builder of a class that contains all information for the gui.
+ * The builder of the {@link DataInput} that contains all information for the gui.
  */
 public interface DataBuilderInput {
     /**
-     * 
-     * @param map
+     * Fluent method that insert a {@link Map} {@link Player} index -> amount of money, in the {@link DataInput}.
+     * @param map to insert.
      * @return this.
      */
     DataBuilderInput cellMap(Map<Integer, Integer> map);
 
     /**
-     * 
-     * @param dices
+     * Fluent method that insert a {@link Pair} of the result of {@link Dices}, in the {@link DataInput}.
+     * @param dices to insert.
      * @return this.
      */
     DataBuilderInput dices(Pair dices);
 
     /**
-     * 
-     * @param event
+     * Fluent method that insert the {@link Event} taken from the {@link MainModel}, in the {@link DataInput}.
+     * @param event to insert.
      * @return this.
      */
     DataBuilderInput event(Event event);
 
     /**
-     * 
-     * @param mode
+     * Fluent method that insert a boolean useful to make decision, in the {@link DataInput}.
+     * @param mode to insert.
      * @return this.
      */
     DataBuilderInput mode(boolean mode);
 
     /**
-     * 
-     * @param text
+     * Fluent method that insert a {@link String} to set some text, in the {@link DataInput}.
+     * @param text to insert.
      * @return this.
      */
     DataBuilderInput text(String text);
 
     /**
-     * 
-     * @param value
+     * Fluent method that insert a {@link Integer} that represents a value to pay, in the {@link DataInput}.
+     * @param value to insert.
      * @return this.
      */
     DataBuilderInput valueToPay(Integer value);
 
     /**
-     * 
+     * The method to return the built product.
      * @return product.
      */
     DataInput build();
