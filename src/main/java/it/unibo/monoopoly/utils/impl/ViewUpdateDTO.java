210e7c6f98af9f2c156a2c33524736e83b619a5d
Federico Mantoni <federico.mantoni@studio.unibo.it>
2025-03-08 12:32:52 +0100

    add javadoc comment to constructor of ViewUpdateDTO

--- src/main/java/it/unibo/monoopoly/utils/impl/ViewUpdateDTO.java
+++ src/main/java/it/unibo/monoopoly/utils/impl/ViewUpdateDTO.java
@@ -25,21 +25,38 @@ import java.util.Optional;
  * @param actualPlayer        the name of the actual {@link Player}
  */
 public record ViewUpdateDTO(
-                Map<String, Integer> playerPositions,
-                Map<Integer, Optional<String>> cellsOwners,
-                Map<Integer, Integer> nBuiltHouses,
-                List<String> prisonedPlayers,
-                List<Integer> mortgagedProperties,
-                Map<String, Integer> playersMoney,
-                String actualPlayer) {
+        Map<String, Integer> playerPositions,
+        Map<Integer, Optional<String>> cellsOwners,
+        Map<Integer, Integer> nBuiltHouses,
+        List<String> prisonedPlayers,
+        List<Integer> mortgagedProperties,
+        Map<String, Integer> playersMoney,
+        String actualPlayer) {
 
+    /**
+     * Explicit constructor to ensure unmodifiable collections are returned.
+     * 
+     * @param playerPositions     map of name of {@link player} in game and its
+     *                            position as a number
+     * @param cellsOwners         map of {@link Buyable} cells expressed as a number
+     *                            and
+     *                            the name of its owner
+     * @param nBuiltHouses        map of {@link Buildable} cell expressed as a
+     *                            number
+     *                            and the number of houses built in it
+     * @param prisonedPlayers     list of the name of the {@link Player}s in prison
+     * @param mortgagedProperties list of the {@link Cell}s mortgaged.
+     * @param playersMoney        the actual money amount of the {@link Player}s in
+     *                            game
+     * @param actualPlayer        the name of the actual {@link Player}
+     */
     public ViewUpdateDTO(final Map<String, Integer> playerPositions,
-    final Map<Integer, Optional<String>> cellsOwners,
-    final Map<Integer, Integer> nBuiltHouses,
-    final List<String> prisonedPlayers,
-    final List<Integer> mortgagedProperties,
-    final Map<String, Integer> playersMoney,
-    final String actualPlayer) {
+            final Map<Integer, Optional<String>> cellsOwners,
+            final Map<Integer, Integer> nBuiltHouses,
+            final List<String> prisonedPlayers,
+            final List<Integer> mortgagedProperties,
+            final Map<String, Integer> playersMoney,
+            final String actualPlayer) {
         this.playerPositions = Collections.unmodifiableMap(playerPositions);
         this.cellsOwners = Collections.unmodifiableMap(cellsOwners);
         this.nBuiltHouses = Collections.unmodifiableMap(nBuiltHouses);
