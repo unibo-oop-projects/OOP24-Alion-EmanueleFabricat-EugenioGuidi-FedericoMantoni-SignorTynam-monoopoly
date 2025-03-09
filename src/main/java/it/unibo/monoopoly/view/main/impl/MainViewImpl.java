6546578ed78898c5081312dc26afea690f2cceae
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2025-03-09 12:25:39 +0100

    remove an import

--- src/main/java/it/unibo/monoopoly/view/main/impl/MainViewImpl.java
+++ src/main/java/it/unibo/monoopoly/view/main/impl/MainViewImpl.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
